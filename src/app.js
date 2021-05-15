205339d62f720fa0b90dc8cbbc70e0b5b7c5cabb
Eyvgeniy <antonm33@yandex.ru>
2021-05-15 11:01:27 +0200

    feat: added error middleware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+const errorMiddleware = require('./middlewares/error-middleware');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -24,5 +25,6 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 boardRouter.use('/:boardId/tasks', taskRouter);
+app.use(errorMiddleware)
 
 module.exports = app;
