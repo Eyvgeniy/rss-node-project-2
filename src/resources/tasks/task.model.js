bf74ba04255bda8078252424a634a208599a874a
Eyvgeniy <antonm33@yandex.ru>
2021-05-29 15:05:33 +0200

    docs: added jsdoc for tasks route

--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -1,13 +1,53 @@
 const { v4: uuid } = require('uuid');
 
+/**
+ * Task Model
+ * @typedef {Object} TaskModel
+ * @property {string} id - User id
+ * @property {string} title - User title
+ * @property {number} order - User order
+ * @property {string} description - User description
+ * @property {string} userId - User userId
+ * @property {string} boardId - User boardId
+ * @property {string} columnId - User columnId *
+ */
+
+/**
+ * Class for create Task
+ */
 class Task {
+  /**
+   *
+   * @param {Object} task Task data
+   */
   constructor({
+    /**
+     * @param {string} id Task id
+     */
     id = uuid(),
+    /**
+     * @param {string} title Task title
+     */
     title = 'title',
+    /**
+     * @param {number} order Task order
+     */
     order = 0,
+    /**
+     * @param {string} description Task description
+     */
     description = 'description',
+    /**
+     * @param {string|null} userId Task userId
+     */
     userId = null,
+    /**
+     * @param {string|null} boardId Task boardId
+     */
     boardId = null,
+    /**
+     * @param {string|null} columnId Task columnId
+     */
     columnId = null,
   } = {}) {
     this.id = id;
@@ -19,6 +59,11 @@ class Task {
     this.columnId = columnId;
   }
 
+  /**
+   * Return task params
+   * @param {TaskModel} task
+   * @returns {TaskModel} Task for responce
+   */
   static toResponse(task) {
     const { id, title, order, description, userId, boardId, columnId } = task;
     return {
