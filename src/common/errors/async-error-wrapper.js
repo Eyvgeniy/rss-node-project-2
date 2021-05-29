e7031babe6daa6ceaf6135c406c25b8b1aa49b82
Eyvgeniy <antonm33@yandex.ru>
2021-05-29 19:10:03 +0200

    docs: added types for utils middlewares

--- src/common/errors/async-error-wrapper.js
+++ src/common/errors/async-error-wrapper.js
@@ -1,3 +1,16 @@
-const wrapAsync = (fn) => (req, res, next) => fn(req, res).catch(next);
+/**
+ * Async wrapper to redirect error from router to error middleware
+ * @param {function} fn Function to wrap
+ * @returns {function} Express middleware
+ */
+const wrapAsync = (fn) =>
+  /**
+   * Express middleware
+   * @param {Object} req Express Request
+   * @param {Object} res Express Responce
+   * @param {function} next Express next function
+   * @returns {Promise}
+   */
+  (req, res, next) => fn(req, res).catch(next);
 
 module.exports = wrapAsync;
--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -1,4 +1,11 @@
 /* eslint-disable no-unused-vars */
+/**
+ * Expree Error middleware
+ * @param {Object} err Express error object
+ * @param {Object} req express Request object
+ * @param {Object} res Express Resonce object
+ * @param {function} next Express next function
+ */
 const errorMiddleware = (err, req, res, next) => {
   res.status(404).send(err.message);
   next();
