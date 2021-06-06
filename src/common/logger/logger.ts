563a5c6ecd14186402e6d5f75e5336255259e4ed
Eyvgeniy <antonm33@yandex.ru>
2021-06-06 20:33:45 +0200

    feat: added save logs to files

--- src/common/logger/logger.ts
+++ src/common/logger/logger.ts
@@ -1,3 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import config from '../config';
+
+const requestLogsPath = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  config.REQUEST_LOGS
+);
+console.log(requestLogsPath);
+const errorsLogsPath = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  config.ERRORS_LOGS
+);
+
 const info = (
   method: string,
   url: string,
@@ -5,10 +25,16 @@ const info = (
   statusCode: number,
   ms: number
 ): void => {
+  fs.writeFileSync(
+    requestLogsPath,
+    `${method} ${url} ${query} ${statusCode} [${ms}ms]\n`,
+    { flag: 'a' }
+  );
   console.log(`${method} ${url} ${query} ${statusCode} [${ms}ms]`);
 };
 
 const error = (stack: string): void => {
+  fs.writeFileSync(errorsLogsPath, `${stack}\n`, { flag: 'a' });
   console.error(`${stack}`);
 };
 
